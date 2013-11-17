66a8b60c34232197ee7ca397f373ee2abb22eaa1
Vikrant Singhal <vikrantsinghal@email.arizona.edu>
2013-11-16 17:42:15 -0800

    Tests being written down.

--- src/housing/test/RenterOwnerTest.java
+++ src/housing/test/RenterOwnerTest.java
@@ -1,5 +1,7 @@
 package housing.test;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
 import housing.House;
 import housing.Owner;
 import housing.Renter;
@@ -11,13 +13,32 @@ public class RenterOwnerTest {
 	Renter r2;
 	Owner o;
 	
+	@Test
+	public void test() {
+		setUp();
+		
+		assertFalse(r1.isRentDue());
+		assertFalse(r2.isRentDue());
+		r1.rentReminder();
+		assertTrue(r1.isRentDue());
+	}
+	
 	public void setUp() {
 		h1 = new House(1);
 		r1 = new Renter(200.0);
 		h2 = new House(2);
 		r2 = new Renter(200.0);
 		o = new Owner();
+		
 		h1.setOccupant(r1);
+		h1.setItemsWithoutGui();
+		h2.setOccupant(r2);
+		h2.setItemsWithoutGui();
+		
 		o.addHouse(h1, r1);
+		o.addHouse(h2, r2);
+		
+		r1.setOwner(o);
+		r2.setOwner(o);
 	}
 }
