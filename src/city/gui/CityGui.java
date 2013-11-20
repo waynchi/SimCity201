6d0cac20c96421e73e29fad8ce7555dddf659aa7
Zack Tanner <zacktanner@gmail.com>
2013-11-20 11:52:15 -0800

    city updates

--- src/city/gui/CityGui.java
+++ src/city/gui/CityGui.java
@@ -1,20 +1,62 @@
 package city.gui;
 import javax.swing.*;
+
+import people.PeopleAgent;
+import restaurant.NormalWaiterRole;
+import restaurant.gui.WaiterGui;
+
 import java.awt.*;
-import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
+
 
 public class CityGui extends JFrame {
 	CityPanel cityPanel;
 	JPanel buildingPanels;
 	CardLayout cardLayout;
 	CityControls cityControls;
+	ArrayList<String> configParams = new ArrayList<String>();
+	NormalWaiterRole normalWaiterRole = new NormalWaiterRole("Waiter");
+	WaiterGui g = new WaiterGui(normalWaiterRole);
 	
 	public CityGui() {
+		FileReader input;
+		try {
+			input = new FileReader("/Users/zacktanner/EclipseProjects/team30/src/config.txt");
+			BufferedReader bufRead = new BufferedReader(input);
+			String line = null;
+			while((line = bufRead.readLine()) != null) {
+				configParams.addAll(Arrays.asList(line.split("\\s*,\\s*")));
+			}
+			for(String item : configParams) {
+				if(isInteger(item)) {
+					int currIndex = configParams.indexOf(item);
+					PeopleAgent person = new PeopleAgent(1000, 0);
+					person.startThread();
+					normalWaiterRole.setGui(g);
+					person.addJob("Waiter", 0, 1200);
+					person.addRole(normalWaiterRole,"Waiter");
+					person.msgTimeIs(0);
+					
+					
+				}
+			}
+			bufRead.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		setVisible( true );
-		setSize( 1024, 760 );
+		setSize( 1024, 1000 );
 		
-		setLayout( new BorderLayout() );
+		getContentPane().setLayout( new BorderLayout() );
 		
 		cityPanel = new CityPanel();
 		cityPanel.setPreferredSize( new Dimension(500, 250) );
@@ -27,7 +69,7 @@ public class CityGui extends JFrame {
 		buildingPanels.setLayout( cardLayout );
 		buildingPanels.setMinimumSize( new Dimension( 500, 250 ) );
 		buildingPanels.setMaximumSize( new Dimension( 500, 250 ) );
-		buildingPanels.setPreferredSize( new Dimension( 500, 250 ) );
+		buildingPanels.setPreferredSize( new Dimension(500, 450) );
 		buildingPanels.setBackground(Color.yellow);
 		
 		cityControls = new CityControls();
@@ -40,9 +82,9 @@ public class CityGui extends JFrame {
 			b.setBuildingPanel( bp );
 			buildingPanels.add( bp, "" + i );
 		}
-		add( BorderLayout.EAST, cityControls);
-		add( BorderLayout.NORTH, cityPanel );
-		add( BorderLayout.SOUTH, buildingPanels );
+		getContentPane().add( BorderLayout.EAST, cityControls);
+		getContentPane().add( BorderLayout.NORTH, cityPanel );
+		getContentPane().add( BorderLayout.SOUTH, buildingPanels );
 	}
 	
 	public void displayBuildingPanel( BuildingPanel bp ) {
@@ -54,5 +96,15 @@ public class CityGui extends JFrame {
 	public static void main( String[] args ) {
 		CityGui sc = new CityGui();
 		
+	}
+	
+	public static boolean isInteger(String s) {
+		 try { 
+		        Integer.parseInt(s); 
+		    } catch(NumberFormatException e) { 
+		        return false; 
+		    }
+		    // only got here if we didn't return false
+		return true;
 	}
 }
