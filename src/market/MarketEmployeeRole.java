8a94a277c7ba07188b74f50d818ef035373fc081
Yinyi Chen <yinyic@usc.edu>
2013-11-21 02:31:37 -0800

    changed method signatures for market roles

--- src/market/MarketEmployeeRole.java
+++ src/market/MarketEmployeeRole.java
@@ -4,69 +4,76 @@ import java.util.List;
 
 public class MarketEmployeeRole {
 	// data
-	enum orderState = {New,OrderFulfilled,ReadyToPay,CheckDelivered,PaymentReceived,OrderPaid,DeliveringChange};
+	enum orderState {New,OrderFulfilled,ReadyToPay,CheckDelivered,PaymentReceived,OrderPaid,DeliveringChange};
 	List<MarketTruckRole> trucks;
 	List<Order> orders;
-	List<Checks> checks;
-	
+	//List<Checks> checks;
+
 	class Order {
-		enum state;
+		orderState state;
 		List<Item> items;
 		double totalDue;
 		double totalPaid;
 		double totalChange;
-		Customer customer;
+		MarketCustomerRole customer;
 		boolean customerAtRestaurant;
-	};
-	
+	}
+
 	class Item {
 		String name;
 		double price;
-	};
+	}
 	
-	
+	private Boolean isActive;
+	private MarketCashierRole cashier;
+
+
 	// messages
-	public void msgHereIsAnOrder (List<Integer>chosenItems, boolean isAtRestaurant) {
-		newOrder = new Order(order);
+	public void msgHereIsAnOrder(List<Integer>chosenItems, boolean isRestaurant) {
+		/*newOrder = new Order(order);
 		newOrder.state = orderState.New;
 		for(item in chosenItems) {
 			newOrder.items.add(item);
 		}
 		newOrder.customerAtRestaurant = isAtRestaurant;
-		orders.add(newOrder);
+		orders.add(newOrder);*/
 	}
-	
+
+
 	public void msgHereIsPayment(double amount, order) {
 		order.totalPaid = order.totalPaid;
 		order.state = orderState.PaymentReceived;		
 	}
+
 	public void msgHereIsChange(double change, order) {
 		order.totalChange = change;
 		order.state = orderState.DeliveringChange;
 	}
-	
+
 	// scheduler
-	for order in orders {
-		if order.state = orderState.New {
-			getOrder(order);
-		}
-		if order.state = orderState.OrderFulfilled {
-			giveOrderToCustomer(order);
-		}
-		if order.state = orderState.ReadyToPay {
-			giveCheckToCustomer(order);
-		}
-		if order.state = orderState.PaymentReceived {
-			getChangeFromCashier(order);
-		}
-		if(order.state = orderState.DeliveringChange) {
-			deliverChangeToCustomer(order);
+	public boolean pickAndExecuteAnAction() {
+		for (Order o : orders) {
+			if (order.state = orderState.New) {
+				getOrder(order);
+			}
+			if (order.state = orderState.OrderFulfilled) {
+				giveOrderToCustomer(order);
+			}
+			if (order.state = orderState.ReadyToPay) {
+				giveCheckToCustomer(order);
+			}
+			if (order.state = orderState.PaymentReceived) {
+				getChangeFromCashier(order);
+			}
+			if(order.state = orderState.DeliveringChange) {
+				deliverChangeToCustomer(order);
+			}
 		}
 	}
-	
-	
+
+
 	// action
-	getOrder(Order order) {
+	private void getOrder(Order order) { //gui
 		for item in order.items {
 			DoGetItem(order.posX,order.posY);
 		}
@@ -74,43 +81,42 @@ public class MarketEmployeeRole {
 
 	}
 
-	giveOrderToCustomer(order) {
-		DoGoToCustomer();
-		if(order.customerAtRestaurant == true) {
+	
+	// if customer is at the Market, give it the items; otherwise send a truck to its place
+	private void giveOrderToCustomer(Order order) {
+		//DoGoToCustomer(); //gui
+		if(order.customerAtRestaurant) {
 			order.customer.msgHereIsYourOrder(order.items);
-			order.state = ReadyToPay;
-
 		} 
 		else {
 			for truck in trucks where truck is available {
-				truck.msgHereIsAnOrder(order.items,this.location);
-
+				truck.msgHereIsAnOrder(order.items,order.customer.location);
 			}
-			order.state = ReadyToPay;
 		}
+		order.state = ReadyToPay;
 	}
 
 
-	giveCheckToCustomer(Order order) {
-		for item in order.items {
-			double totalDue = totalDue + item.price;
-		}
+	private void giveBillToCashier(Order order) {
 		order.totalDue = totalDue;
-		DoGoToCashier();
-		order.customer.msgHereIsWhatIsDue(order.customer,totalDue);
-		order.state = CheckDelivered;
-	}
-	getChangeFromCashier(order) {
-		DoGoToCashier();
-		cashier.msgHereIsPayment(order.totalPaid, order.totalDue, this);
-		order.state = OrderPaid;
-	}
-	deliverChangeToCustomer(order) {
-		DoGoToCustomer();
-		order.customer.msgHereIsChange(order.totalChange);
+		//DoGoToCashier();
+		cashier.msgHereIsACheck(order.customer, order.items);
+		
 		orders.remove(order);
 	}
-	
-	
+
+	//utilities
+	public Boolean isActive() {
+		return isActive;
+	}
+
+	public void setCashier(MarketCashierRole c) {
+		cashier = c;
+	}
 	
 }
+
+
+
+
+
--- src/market/MarketTruckRole.java
+++ src/market/MarketTruckRole.java
@@ -1,5 +1,27 @@
 package market;
 
+import java.awt.Dimension;
+import java.util.List;
+
 public class MarketTruckRole {
+	//data
+	private Boolean isActive;
+	
+	//messages
+	public void msgHereIsAnOrder(List<Integer> order, Dimension location) {
+		
+	}
+
+	
+	//scheduler
+	
+	
+	//actions
+	
+	
+	//utilities
+	public Boolean isActive() {
+		return isActive;
+	}
 
 }
