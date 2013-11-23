9e51ae9faaaa4141ce116e3c62a0a7100b8ff524
Yinyi Chen <yinyic@usc.edu>
2013-11-22 17:15:48 -0800

    created market mocks

---
+++ src/market/mock/MockMarketCashier.java
@@ -0,0 +1,12 @@
+package market.mock;
+
+import market.interfaces.MarketCashier;
+
+public class MockMarketCashier extends Mock implements MarketCashier{
+
+	public MockMarketCashier(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/market/mock/MockMarketEmployee.java
@@ -0,0 +1,12 @@
+package market.mock;
+
+import market.interfaces.MarketEmployee;
+
+public class MockMarketEmployee extends Mock implements MarketEmployee{
+
+	public MockMarketEmployee(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
