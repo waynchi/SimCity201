3f58154e24966df864f3e9430a4ecd63cf025ed1
Yinyi Chen <yinyic@usc.edu>
2013-11-22 19:09:10 -0800

    completed MarketTruck, created truck gui. close #20

--- src/market/MarketTruckAgent.java
+++ src/market/MarketTruckAgent.java
@@ -1,27 +1,60 @@
 package market;
 
-import java.awt.Dimension;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-public class MarketTruckAgent {
+import agent.Agent;
+import market.gui.MarketTruckGui;
+import market.interfaces.MarketCustomer;
+import market.interfaces.MarketTruck;
+
+public class MarketTruckAgent extends Agent implements MarketTruck{
 	//data
-	private Boolean isActive;
+	MarketTruckGui gui = new MarketTruckGui(this);
+	List<Order> orders = new ArrayList<Order>();
+	
+	class Order {
+		MarketCustomer customer;
+		Map<String,Integer> items;
+		
+		Order (MarketCustomer c, Map<String, Integer> i) {
+			customer = c;
+			items = i;
+		}
+	}
 	
 	//messages
-	public void msgHereIsAnOrder(List<Integer> order, Dimension location) {
-		
+	public void msgHereIsAnOrder(MarketCustomer customer, Map<String,Integer> items) {
+		orders.add(new Order (customer, items));
+		stateChanged();
 	}
 
 	
 	//scheduler
-	
-	
-	//actions
-	
-	
-	//utilities
-	public Boolean isActive() {
-		return isActive;
+	public boolean pickAndExecuteAnAction() {
+		if (!orders.isEmpty()) {
+			deliverOrder(orders.get(0));
+			return true;
+		}
+		return false;
 	}
 
+
+	
+
+	
+	//actions
+	private void deliverOrder(Order order) {
+		gui.deliver(order.customer.getPersonAgent().getPosition(); // need a getGui for personAgent
+		order.customer.msgHereIsYourOrder(order.items);
+		orders.remove(order);
+	}
+	
+	//utilities
+	@Override
+	public boolean isAvailable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
