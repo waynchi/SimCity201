7a7484207d5e69c16bae18ee74e9ee356d1cb7f0
Vikrant Singhal <vikrant.singhal15@gmail.com>
2013-11-22 19:55:15 -0800

    Mock agents exten roles now

--- src/housing/gui/RepairManGui.java
+++ src/housing/gui/RepairManGui.java
@@ -6,19 +6,14 @@ public class RepairManGui implements HGui{
 
 	@Override
 	public void updatePosition() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void draw(Graphics2D g) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public boolean isPresent() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 }
