e77de5edc6f5b1135a5b54e3d8cf835fda86192b
Eric Strode <estrode@usc.edu>
2013-11-22 17:24:13 -0800

    Refactor bank roles

--- src/bank/BankCustomerRole.java
+++ src/bank/BankCustomerRole.java
@@ -12,10 +12,12 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import people.Role;
+
 /**
  * Bank customer agent.
  */
-public class BankCustomerAgent extends Agent implements BankCustomer {
+public class BankCustomerRole extends Role implements BankCustomer {
 	private String name;
 
 	Timer timer = new Timer();
@@ -45,7 +47,7 @@ public class BankCustomerAgent extends Agent implements BankCustomer {
 	 * @param name name of the customer
 	 * @param gui  reference to the customergui so the customer can send it messages
 	 */
-	public BankCustomerAgent(String name){
+	public BankCustomerRole(String name){
 		super();
 		this.name = name;
 	}
