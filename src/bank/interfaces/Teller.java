2a45de4a245b6308e2d70ca555be6e6f0d425137
Eric Strode <estrode@usc.edu>
2013-11-24 15:30:56 -0800

    Update teller interface

--- src/bank/interfaces/Teller.java
+++ src/bank/interfaces/Teller.java
@@ -1,5 +1,8 @@
 package bank.interfaces;
 
+import market.interfaces.MarketCashier;
+import restaurant.interfaces.Cashier;
+
 /**
  * teller
  */
@@ -7,6 +10,10 @@ package bank.interfaces;
 public interface Teller {
 
 	public abstract void msgHere(BankCustomer cust, String name);
+	
+	public abstract void msgNeedHelp(Cashier cashier, String name);
+	
+	public abstract void msgNeedHelp(MarketCashier mcashier, String name);
 	
 	public abstract void msgWithdraw(int accountID, double moneyNeeded);
 	
