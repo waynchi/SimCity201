2dbd50989a0735194d7ec9768f481d60933f18c0
Vikrant Singhal <vikrant.singhal15@gmail.com>
2013-11-24 22:53:12 -0800

    Almost done. Almost

--- src/housing/gui/RepairManGui.java
+++ src/housing/gui/RepairManGui.java
@@ -1,5 +1,7 @@
 package housing.gui;
 
+import housing.House;
+import housing.HouseType;
 import housing.interfaces.RepairMan;
 
 import java.awt.Dimension;
@@ -9,10 +11,17 @@ public class RepairManGui implements HGui{
 	public RepairMan r;
 	public RepairShopGui gui;
 	int xPos, yPos, xDestination, yDestination;
+	public Location location = Location.Shop;
+	public State state = State.Idle;
+	public HouseGui targetGui;
 	
-	public enum State {EnteringShop};
-	public enum Location {};
+	public enum State {Idle, EnteringHouse, LeavingHouse, Repairing, LeavingApartmentComplex};
+	public enum Location {Outside, Shop, ApartmentComplex, Apartment, Villa};
 
+	public RepairManGui(RepairMan r) {
+		this.r = r;
+	}
+	
 	@Override
 	public void updatePosition() {
 		if (xPos < xDestination)
@@ -25,15 +34,57 @@ public class RepairManGui implements HGui{
 			yPos--;
 		
 		if (xPos == xDestination && yPos == yDestination) {
+			if (state == State.EnteringHouse && location == Location.ApartmentComplex) {
+				location = Location.Apartment;
+				state = State.Idle;
+				xPos = targetGui.entranceCoordinatesInternal.width;
+				yPos = targetGui.entranceCoordinatesInternal.height;
+				Dimension d = targetGui.getPosition("DiningTable");
+				goToLocation(d);
+				r.activityDone();
+			}
+			else if (state == State.LeavingHouse) {
+				if (targetGui.h.type == HouseType.Apartment) {
+					location = Location.ApartmentComplex;
+				}
+				else {
+					location = Location.Outside;
+				}
+				state = State.Idle;
+				xPos = targetGui.entranceCoordinatesExternal.width;
+				yPos = targetGui.entranceCoordinatesExternal.height;
+				r.activityDone();
+			}
+			else if (state == State.Repairing) {
+				state = State.Idle;
+				r.activityDone();
+			}
+			else if (state == State.LeavingApartmentComplex && location == Location.ApartmentComplex) {
+				location = Location.Outside;
+				state = State.Idle;
+				r.activityDone();
+			}
 		}
 	}
 
 	@Override
 	public void draw(Graphics2D g) {
+		if (location != Location.Outside) {
+			g.fillOval(xPos, yPos, 15, 15);
+		}
 	}
 
 	@Override
 	public boolean isPresent() {
+		if (location == Location.ApartmentComplex)
+			return false;
+		else
+		return true;
+	}
+	
+	public boolean isPresentInShop() {
+		if (location == Location.Shop)
+			return true;
 		return false;
 	}
 	
@@ -41,10 +92,81 @@ public class RepairManGui implements HGui{
 	}
 	
 	public void DoEnterShop() {
+		location = Location.Shop;
+		state = State.Idle;
+		xPos = gui.entranceCoordinatesInternal.width;
+		yPos = gui.entranceCoordinatesInternal.height;
+		Dimension d = gui.getPosition("Chair");
+		goToLocation(d);
+		r.activityDone();
 	}
 	
-	private void goToLocation(Dimension d) {
+	public void DoEnterHouse(HouseGui g) {
+		House h = g.h;
+		if (h.type == HouseType.Apartment) {
+			state = State.EnteringHouse;
+			location = Location.ApartmentComplex;
+			xPos = g.h.a.gui.entranceCoordinates.width;
+			yPos = g.h.a.gui.entranceCoordinates.height;
+			Dimension d = g.entranceCoordinatesExternal;
+			goToLocation(d);
+		}
+		else {
+			state = State.Idle;
+			Dimension d = g.getPosition("DiningTable");
+			goToLocation(d);
+			r.activityDone();
+		}
+		targetGui = g;
+	}
+	
+	public void DoLeaveHouse() {
+		state = State.LeavingHouse;
+		goToLocation(targetGui.entranceCoordinatesInternal);
+	}
+	
+	public void goToLocation(Dimension d) {
 		xDestination = d.width;
 		yDestination = d.height;
+	}
+	
+	public void DoRepairItem(Dimension d) {
+		state = State.Repairing;
+		xDestination = d.width;
+		yDestination = d.height;
+	}
+	
+	public void DoGoToApartmentInSamePlaceFromApartment(HouseGui g) {
+		this.targetGui = g;
+		location = Location.ApartmentComplex;
+		state = State.EnteringHouse;
+		goToLocation(targetGui.entranceCoordinatesExternal);
+	}
+	
+	public void DoLeaveApartmentComplexToFixFromApartment(HouseGui g) {
+		state = State.LeavingApartmentComplex;
+		location = Location.ApartmentComplex;
+		goToLocation(targetGui.h.a.gui.entranceCoordinates);
+		this.targetGui = g;
+	}
+	
+	public void DoGoToHouseInDifferentPlaceToFixFromVilla(HouseGui g) {
+		targetGui = g;
+	}
+	
+	public void DoGoToVillaFromVilla(HouseGui g) {
+		targetGui = g;
+	}
+	
+	public void DoReturnToShop() {
+		if (targetGui.h.type == HouseType.Apartment) {
+			goToLocation(targetGui.h.a.gui.entranceCoordinates);
+			state = State.LeavingApartmentComplex;
+			location = Location.ApartmentComplex;
+		}
+		else {
+			state = State.Idle;
+			location = Location.Outside;
+		}
 	}
 }
