3b254312ef21f82c19edbebeff7744f8890e45f7
Wayne Chi <waynchi@gmail.com>
2013-11-25 00:51:05 -0800

    Fixed Flickering

--- src/city/gui/CityGui.java
+++ src/city/gui/CityGui.java
@@ -70,7 +70,7 @@ public class CityGui extends JFrame implements ActionListener {
 
 		FileReader input;
 		try {
-			input = new FileReader("config.txt");
+			input = new FileReader("src\\config.txt");
 			BufferedReader bufRead = new BufferedReader(input);
 			String line = null;
 			while ((line = bufRead.readLine()) != null) {
@@ -214,7 +214,7 @@ public class CityGui extends JFrame implements ActionListener {
 		if(time == 12000) {
 			time=0;
 		}
-		repaint();
+		//repaint();
 
 	}
 }
