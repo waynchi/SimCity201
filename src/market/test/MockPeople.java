e02fe6c2abde05a9ea0adf583eafa1a3b464346c
Yinyi Chen <yinyic@usc.edu>
2013-11-25 01:33:53 -0800

    updated mock people and test

---
+++ src/market/test/MockPeople.java
@@ -0,0 +1,179 @@
+package market.test;
+
+import java.util.List;
+
+import city.gui.CityGui;
+import city.gui.PersonGui;
+import people.People;
+import people.Role;
+
+public class MockPeople extends Mock implements People{
+
+	public MockPeople(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double getMoney() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setMoney(double Money) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Role> getRoles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAgentState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAgentEvent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHunger() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Role getHost(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Role getTeller() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Role getMarketEmployee() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getMaitreDName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPersonGui(PersonGui gui) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setCityGui(CityGui gui) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Arrived() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addRole(Role r, String description) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void CallPrint(String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void CallDo(String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addJob(String job, int start, int end) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgDone(String role) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgTimeIs(int Time) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void GoToRestaurant() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void GoToHouse() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void GoBuyCar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void LeaveWork() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void GoToBank() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void GoToWork() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void CallstateChanged() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int msgWhatIsTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
