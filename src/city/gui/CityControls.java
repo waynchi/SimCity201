11d3bba41b954fd2ee71bc3bb0cd6bda9fa0bbf9
Zack Tanner <zacktanner@gmail.com>
2013-11-25 14:27:35 -0800

    added tabs

--- src/city/gui/CityControls.java
+++ src/city/gui/CityControls.java
@@ -13,16 +13,46 @@ public class CityControls extends JPanel implements ActionListener {
 	private JTextField textField_1;
 	public CityControls() {
 		
-		textField_1 = new JTextField();
-		add(textField_1);
-		textField_1.setColumns(10);
 		
-		JButton btnAddPerson = new JButton("Add Person");
-		add(btnAddPerson);
+		// Create and set up the window.
+		final JFrame frame = new JFrame("Split Pane Example");
+
+		// Display the window.
+		this.setSize(500, 300);
+		this.setVisible(true);
+
+		// set grid layout for the frame
+
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+
+		tabbedPane.addTab("Tab1", makePanel("People"));
+		tabbedPane.addTab("Tab2", makePanel("Other"));
+
+		this.add(tabbedPane);
 		
-		setBackground( Color.ORANGE );
-		setPreferredSize( new Dimension(200, 77) );
 		
+		
+		
+	}
+	
+	private JPanel makePanel(String text) {
+		if(text.equals("People")) {
+			JPanel panel = new JPanel();
+			textField_1 = new JTextField();
+			panel.add(textField_1);
+			textField_1.setColumns(10);
+			
+			JButton btnAddPerson = new JButton("Add Person");
+			panel.add(btnAddPerson);
+			
+			panel.setBackground( Color.ORANGE );
+			panel.setPreferredSize( new Dimension(200, 77) );
+			return panel;
+
+		} else {
+			JPanel panel = new JPanel();
+			return panel;
+		}
 	}
 
 	@Override
