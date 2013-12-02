86d18cf0819cbf5ec83e7b819597472a4cd5f01e
Vikrant Singhal <vikrant.singhal15@gmail.com>
2013-12-01 19:33:10 -0800

    Minore change

--- src/housing/gui/ApartmentsGui.java
+++ src/housing/gui/ApartmentsGui.java
@@ -13,7 +13,7 @@ public class ApartmentsGui implements HGui{
 	public Apartments a;
 	public int houseWidth = 76;
 	public int houseHeight = 76;
-	public Dimension entranceCoordinates = new Dimension(250, 530);
+	public Dimension entranceCoordinates = new Dimension(197, 520);
 	List<HGui> guis = new ArrayList<HGui>();
 	public ApartmentsAnimationPanel ap;
 
