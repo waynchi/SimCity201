48aed2df39dbdf0d5382bcc9cd804a70d1d3de73
Zack Tanner <zacktanner@gmail.com>
2013-12-07 19:09:06 -0800

    removed transportation spam

--- src/transportation/BusAgent.java
+++ src/transportation/BusAgent.java
@@ -41,7 +41,7 @@ public void msgNumberOfBoardingPassengers(int size) {
 
 @Override
 public void msgImBoarding(BusPassenger p){ //remove place
-System.out.println("Bus recieved message that passenger is boarding");
+//System.out.println("Bus recieved message that passenger is boarding");
 myBusPassengers.add(new Passenger(p));
 boardingPassengers--;
 	if(boardingPassengers == 0)
@@ -57,7 +57,7 @@ boardingPassengers--;
  */
 @Override
 public void msgImLeaving(BusPassenger p){
-	System.out.println("Bus recieved message that passenger is leaving");
+	//System.out.println("Bus recieved message that passenger is leaving");
 Passenger toRemove = findPassenger(p);
 myBusPassengers.remove(toRemove);
 }
