8f726df98f836fdc5b80f693172f393de5e8ee9c
Wayne Chi <waynchi@gmail.com>
2013-12-07 22:40:33 -0800

    ADFSDFASDF

--- src/people/PeopleAgent.java
+++ src/people/PeopleAgent.java
@@ -582,10 +582,10 @@ public class PeopleAgent extends Agent implements People{
 	//scheduler
 	@Override
 	public boolean pickAndExecuteAnAction() {
-		print("My Current State is: " + state.toString());
-		print("My Current Event is: " + event.toString());
-		print("My Current Hunger is : " + hunger.toString());
-		print("My Type is: " + type);
+//		print("My Current State is: " + state.toString());
+//		print("My Current Event is: " + event.toString());
+//		print("My Current Hunger is : " + hunger.toString());
+//		print("My Type is: " + type);
 		boolean Roles = false, Person = false;
 		if(type.equals("default"))
 		{
