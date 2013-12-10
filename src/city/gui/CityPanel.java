77ba3f6830c0d7fbce9a64bbe8d9e1832924db20
Zack Tanner <zacktanner@gmail.com>
2013-12-09 23:18:07 -0800

    removed debug

--- src/city/gui/CityPanel.java
+++ src/city/gui/CityPanel.java
@@ -766,12 +766,12 @@ public class CityPanel extends JPanel implements MouseListener {
 //		bpr.setDestinationBusStop(busStops.get(0));
 //		bpr.msgIsActive();
 		
-		CarGui cg = new CarGui(5, 5, 10, 10, road2, road2.get(0), allRoads, this);
-		CarAgent carAgent = new CarAgent();
-		carAgent.setGui(cg);
-		carAgent.startThread();
-		cg.msgGoToThisPlace(carAgent, "Apartment 2");
-		vehicles.add(cg);
+//		CarGui cg = new CarGui(5, 5, 10, 10, road2, road2.get(0), allRoads, this);
+//		CarAgent carAgent = new CarAgent();
+//		carAgent.setGui(cg);
+//		carAgent.startThread();
+//		cg.msgGoToThisPlace(carAgent, "Apartment 2");
+//		vehicles.add(cg);
 //		
 
 		
